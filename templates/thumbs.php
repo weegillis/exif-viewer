e906fabb019136273f07857f997a6b58be73378d
weegillis <roy.pierce@gmail.com>
2015-01-09 03:01:40 -0700

    Create thumbs.php
    2012 thumbs template

---
+++ templates/thumbs.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="UTF-8">
+<title><?php ins($title); ?> &bull; photo navigator</title>
+<meta name="robots" content="noindex,nofollow">
+<link href="/css/exif.css" rel="stylesheet">
+</head>
+<body>
+<div id="wrap">
+<?php getEXIF($imgDir); ?>
+</div>
+</body>
+</html>
